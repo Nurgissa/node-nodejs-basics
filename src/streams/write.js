99de13044bf183f88e4a8d2565e50cf86c795bee
Nurgissa <nurgisa@gmail.com>
2024-10-08 01:15:42 +0200

    feat: implement stream tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import path from "path";
+import {fileURLToPath} from "url";
+import fs from "fs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const file = path.join(__dirname, '/files/fileToWrite.txt');
+    const ws = fs.createWriteStream(file);
+    const rs = process.stdin.pipe(ws);
 };
 
 await write();

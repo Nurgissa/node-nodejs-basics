a86b88f5817c98acd85177921dfded343cf6d55a
Nurgissa <nurgisa@gmail.com>
2024-10-08 00:19:33 +0200

    feat: implement CLI tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    let result = [];
+    const args = process.argv;
+
+    for (let i = 2; i < args.length; i += 2 ) {
+        const key = args[i].replace(/--/g, '');
+        const value = args[i+1];
+
+        result.push(`${key} is ${value}`);
+    }
+    console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+
+    const suitableEnvs = Object.entries(process.env).filter(([k, v]) => k.toUpperCase().startsWith("RSS_"));
+
+    const envs = []
+    for (const [key, value] of suitableEnvs) {
+        envs.push(`${key}=${value}`);
+    }
+    console.log(envs.join("; "));
 };
+
+
 
 parseEnv();
